476d43fa3d1843ad21b5d0c3bdaf3a76660adcd3
Marco Scibetta <marco.scibetta@eng.it>
2024-01-08 17:04:09 +0100

    fetchDepartmentById test method

--- src/test/java/com/keelient/springboot/tutorial/controller/DepartmentControllerTest.java
+++ src/test/java/com/keelient/springboot/tutorial/controller/DepartmentControllerTest.java
@@ -1,7 +1,6 @@
 package com.keelient.springboot.tutorial.controller;
 
 import com.keelient.springboot.tutorial.entity.Department;
-import com.keelient.springboot.tutorial.error.DepartmentNotFoundException;
 import com.keelient.springboot.tutorial.service.DepartmentService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,14 +10,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(DepartmentController.class)
@@ -61,6 +57,19 @@ class DepartmentControllerTest {
                                 "\t\"departmentCode\":\"IT-06\"\n" +
                                 "}"))
                 .andExpect(status().isOk());
+    }
+
+
+    @Test
+    void fetchDepartmentById() throws Exception {
+        Mockito.when(departmentService.fetchDepartmentById(1L))
+                .thenReturn(Optional.ofNullable(department));
+
+        mockMvc.perform(get("/api/v1/departments/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.departmentName").
+                        value(department.getDepartmentName()));
     }
 
     @Test
